4194169f66ba0af71f92be8886e8900bf13f0477
makajusazeena <makaju.sazeena@gmail.com>
2023-07-12 12:20:47 +0545

    enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
-Fancybox.bind('[data-fancybox="gallery"]', {
-//
-});
+// let mobBtn = document.getElementById('#sm-mob-btn');
+// let navbar = document.querySelector('.sm-header .sm-navbar');
+// if(mobBtn){
+// 	mobBtn.onclick = () => {
+// 		mobBtn.classList.toggle('fa-times');
+// 		navbar.classList.toggle('active');
+// 	};
+
+// }
+
+// window.onscroll = () => {
+// 	mobBtn.classList.remove('fa-times');
+// 	navbar.classList.remove('active');
+// };
+
+jQuery(document).ready(function($) {
+	var mobBtn = $('.sm-btn-wrap #sm-mob-btn');
+	var navbar = $('.sm-header .sm-navbar');
+	if(mobBtn){
+		mobBtn.click(function(){
+			// alert('here');
+			mobBtn.toggleClass('fa-times');
+			navbar.toggleClass('active');
+		})
+	}
+})
