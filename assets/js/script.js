1ffa0abd33cf7f20fbae469fd6a08d5488477195
makajusazeena <108604419+makajusazeena@users.noreply.github.com>
2025-02-28 16:42:37 +0545

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,46 @@
 // 	navbar.classList.remove('active');
 // };
 
-jQuery(document).ready(function($) {
-	var mobBtn = $('.sm-btn-wrap #sm-mob-btn');
-	var navbar = $('.sm-header .sm-navbar');
-	if(mobBtn){
-		mobBtn.click(function(){
-			// alert('here');
-			mobBtn.toggleClass('fa-times');
-			navbar.toggleClass('active');
-		})
-	}
-})
+// ***** typing animation ****/
+// var typed = new Typed(".typing", {
+//   strings: [
+//     "",
+//     "Web Developer",
+//     "Web Designer",
+//     "Graphic Designer",
+//     "Youtuber",
+//   ],
+//   typeSpeed: 100,
+//   BackSpeed: 60,
+//   loop: true,
+// });
+
+document.addEventListener("scroll", function () {
+  const flipContainer = document.querySelector(".flip-container");
+  const cardBody = document.querySelector(".sm-card-body");
+
+  const cardBodyRect = cardBody.getBoundingClientRect();
+  const cardBodyMiddle = cardBodyRect.top + cardBodyRect.height * 0.6; // 60% of card body
+
+  const windowHeight = window.innerHeight;
+
+  // Step 1: Flip after passing slightly more than the middle of sm-card-body
+  if (cardBodyMiddle < windowHeight * 0.5) {
+    // Flip after middle + extra
+    flipContainer.classList.add("flipped");
+  } else {
+    flipContainer.classList.remove("flipped"); // Flip back when scrolling up
+  }
+});
+
+// jQuery(document).ready(function ($) {
+//   var mobBtn = $(".sm-btn-wrap #sm-mob-btn");
+//   var navbar = $(".sm-header .sm-navbar");
+//   if (mobBtn) {
+//     mobBtn.click(function () {
+//       // alert('here');
+//       mobBtn.toggleClass("fa-times");
+//       navbar.toggleClass("active");
+//     });
+//   }
+// });
