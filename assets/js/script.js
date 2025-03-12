ec45477479b20b77cced60cccad0942265e3bbc3
makajusazeena <108604419+makajusazeena@users.noreply.github.com>
2025-03-12 15:48:41 +0545

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,12 +30,9 @@
 document.addEventListener("scroll", function () {
   const flipContainer = document.querySelector(".flip-container");
   const cardBody = document.querySelector(".sm-card-body");
-
   const cardBodyRect = cardBody.getBoundingClientRect();
   const cardBodyMiddle = cardBodyRect.top + cardBodyRect.height * 0.4; // 50% of card body
-
   const windowHeight = window.innerHeight;
-
   // Step 1: Flip after passing slightly more than the middle of sm-card-body
   if (cardBodyMiddle < windowHeight * 0.5) {
     // Flip after middle + extra
